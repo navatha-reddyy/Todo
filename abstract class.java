a4f05ba56a6cae85dc043ac4bb3bfc5d09fc351f
navatha-reddyy <nareddynavathareddy@gmail.com>
2024-11-29 11:01:11 +0530

    Add files via upload

---
+++ abstract class.java
@@ -0,0 +1,35 @@
+Abstract class figure
+{
+int d1,d2;
+figure(int x,int y)
+{d1=x;d2=y;}
+void area()
+{
+system.out.println("area intialized");
+}
+}
+class Rectangle extends figure
+{
+Rectangle(int x,int y);super(x,y);
+void area()
+{
+system.out.println("area of Rect"+(d1*d2);}
+}
+class Triangle extends figure
+{
+Triangle(int x,int y);super(x,y);
+void area()
+{
+{
+system.out.println("area of triangle"+(1/2*d1*d2);}
+}
+}
+class triangle rectangle{
+publice static void main()
+{
+rectangle R=new rectangle(10,20);
+R area();
+triangle t=new triangle(10,20);
+T area();
+}
+}
